933b50963bf321e98bc83c92e0262083bdcf3419
Rainer Ilgen <rainerilgen@yahoo.ca>
2016-12-30 09:14:13 -0700

    Modified CHIP basic install script to use custom blink.cfg

--- scripts/chip_install.sh
+++ scripts/chip_install.sh
@@ -11,7 +11,7 @@ wget -O /usr/local/bin/gpio.sh http://fordsfords.github.io/gpio_sh/gpio.sh
 wget -O /usr/local/bin/blink.sh http://fordsfords.github.io/blink/blink.sh
 chmod +x /usr/local/bin/blink.sh
 wget -O /etc/systemd/system/blink.service http://fordsfords.github.io/blink/blink.service
-wget -O /usr/local/etc/blink.cfg http://fordsfords.github.io/blink/blink.cfg
+wget -O /usr/local/etc/blink.cfg https://raw.githubusercontent.com/rainerilgen/CHIP-Files/master/config/blink.cfg
 systemctl enable /etc/systemd/system/blink.service
 systemctl daemon-reload
 service blink start
