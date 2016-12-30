d953ae8dce514c7706640ce21de66f30c6be727c
Rainer Ilgen <rainer@Command>
2016-12-30 08:31:02 -0700

    Added Logitech Media Server insall script

---
+++ scripts/chip_lms_install.sh
@@ -0,0 +1,16 @@
+echo "Installing Logitech Media Server"
+apt-get update
+apt-get install -y cifs-utils
+mkdir /media/music
+mkdir /media/playlists
+echo "//192.168.178.10/Music  /media/music  cifs  guest,uid=1000,iocharset=utf8  0  0" >> /etc/fstab
+echo "//192.168.178.10/playlists  /media/playlists  cifs  guest,uid=1000,iocharset=utf8  0  0" >> /etc/fstab
+mount -a
+cd /tmp
+wget http://downloads-origin.slimdevices.com/nightly/7.9/sc/56ee601/logitechmediaserver_7.9.0~1480516903_arm.deb
+dpkg -i logitechmediaserver_7.9.0~1480516903_arm.deb
+
+echo "https://github.com/disaster123/shairport2_plugin"
+apt-get install -y libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils libio-socket-ssl-perl
+wget http://www.inf.udec.cl/~diegocaro/rpi/libnet-sdp-perl_0.07-1_all.deb
+dpkg -i libnet-sdp-perl_0.07-1_all.deb
